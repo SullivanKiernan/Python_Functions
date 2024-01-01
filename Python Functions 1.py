166df3c37fb59dc5faf761e93f75b7bfe4ea1da6
SullivanKiernan <151576937+SullivanKiernan@users.noreply.github.com>
2024-01-01 10:28:56 -0500

    Add files via upload

---
+++ Python Functions 1.py
@@ -0,0 +1,6 @@
+def greet(First_Name, Last_Name):
+    print(f"Hi {First_Name} {Last_Name}")
+    print("Welcome aboard")
+
+
+greet(First_Name = input("First Name? "), Last_Name = input("Last Name? "))
